aa698f95b12d53bb771bc31de5ceeb8d2afd7f82
Mikhail <mikssur@gmail.com>
2022-11-14 12:51:30 +0200

    Add procfile

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 5000
+const port = process.env.PORT || 5000
 const Connection = require('./lib/mongo').Connection
 const bodyParser = require('body-parser')
 const { check } = require('express-validator')
